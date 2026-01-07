90e0875cea48eb5ef7ccc3d6d3d7032a70784263
Red691 <kumarnikhil05848@gmail.com>
2026-01-07 14:36:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from os import environ
 import logging
 from logging.handlers import RotatingFileHandler
 
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7280187591:AAG-aaAesc20QDzvu1G2SCU_xq5MQHVMB68")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7280187591:AAE-Lh16r1noxlMGeRn0y7SYhj3XjkT6NOs")
 BOT_USERNAME = 'New_Movie_filter_bot'
 APP_ID = int(os.environ.get("APP_ID", "20594537"))
 API_HASH = os.environ.get("API_HASH", "c505a4e5bb7d482197875888af544f17")
